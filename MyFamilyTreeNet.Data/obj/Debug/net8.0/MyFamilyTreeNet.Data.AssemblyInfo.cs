fc4153e037531386b62c7e44484b161d5a5f1ad6
Mariyan Yordanov <marsrewq@gmail.com>
2025-08-19 22:27:20 +0300

    теst family functions

--- MyFamilyTreeNet.Data/obj/Debug/net8.0/MyFamilyTreeNet.Data.AssemblyInfo.cs
+++ MyFamilyTreeNet.Data/obj/Debug/net8.0/MyFamilyTreeNet.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFamilyTreeNet.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9f7d9a9786ba39fc114c2df090570b85c07c978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c093aaf9683aed057c3a428bd39166ccf6e5ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFamilyTreeNet.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFamilyTreeNet.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
